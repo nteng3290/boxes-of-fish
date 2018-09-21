204ac80de9b3cdd3ead29cdfcf7ef55aa799d8a0
Natasha Teng <nteng3290@gmail.com>
2018-09-21 11:13:50 -0400

    added button component and social icons

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Button from './components/Button';
+import Social from "./components/Social"
 
 class App extends React.Component {
     render() {
+      const SocialIcons = ["google", "facebook", "twitter"];
       return (
         <div>
-          Hello
+          <h1>AcmeStack</h1>
+          <Button btn-type="Login" />
+          <div className="social-media-container">
+            { 
+              SocialIcons.map(social => (
+                <Social type={social} key={social}/>
+              ))
+            }
+          </div>
         </div>
       )
     }
