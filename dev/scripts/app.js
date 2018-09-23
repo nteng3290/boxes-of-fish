c95d04384f0d23b0bac9a3d7a1dd0544a5dba10c
Natasha Teng <nteng3290@gmail.com>
2018-09-23 15:21:15 -0400

    Created login components & basic css

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,23 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from './components/Button';
-import Social from "./components/Social"
+// import Social from './components/Social';
+import Input from './components/Input';
+import Facebook from './components/Facebook';
+import Google from './components/Google';
+import Twitter from './components/Twitter';
 
 class App extends React.Component {
+    constructor() {
+      super();
+      this.state={
+        loggedIn : false,
+      }
+      this.loginUpdate = this.loginUpdate.bind(this);
+    };
+    loginUpdate(e){
+      e.preventDefault();
+      this.setState({
+        loggedIn : !this.state.loggedIn,
+      })
+        
+    }
+    checkPassword(){
+
+    }
     render() {
-      const SocialIcons = ["google", "facebook", "twitter"];
+      // const SocialIcons = ["google", "facebook", "twitter"];
       return (
-        <div>
-          <h1>AcmeStack</h1>
-          <Button btn-type="Login" />
-          <div className="social-media-container">
-            { 
-              SocialIcons.map(social => (
-                <Social type={social} key={social}/>
-              ))
-            }
+        <React.Fragment>
+          <div className="logo">
+            <h1>AcmeStack</h1>
+            <div className="logo-icon">
+              <img src="/images/svg/logo-acmestack.svg" alt="AcmeStack Logo"/>
+            </div>
           </div>
-        </div>
+          <div className="login">
+            <form className="login-form">
+              <Input type="text" name="username"/>
+              <Input type="password" name="password"/>
+              <Button btnType="Login" click={this.loginUpdate}/>
+            </form>
+            <Input type="checkbox" name="remember me"/>
+            <div className="social-media-container">
+              <Google />
+              <Facebook />
+              <Twitter />
+            </div>
+          </div>
+        </React.Fragment>
       )
     }
 }
