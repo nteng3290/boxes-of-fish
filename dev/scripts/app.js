d4365996e5e804aae21820259393f49e4ff4cf4a
Natasha Teng <nteng3290@gmail.com>
2018-09-23 20:11:52 -0400

    spacing and added login updates

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from './components/Button';
-// import Social from './components/Social';
 import Input from './components/Input';
 import Facebook from './components/Facebook';
 import Google from './components/Google';
@@ -13,43 +12,43 @@ class App extends React.Component {
       this.state={
         loggedIn : false,
       }
+      this.passwordInput = React.createRef();
       this.loginUpdate = this.loginUpdate.bind(this);
     };
     loginUpdate(e){
+      //Check User login
       e.preventDefault();
       this.setState({
         loggedIn : !this.state.loggedIn,
       })
-        
     }
     checkPassword(){
-
+      //Check length of password
+      this.passwordInput;
+      console.log(this.passwordInput);
     }
     render() {
-      // const SocialIcons = ["google", "facebook", "twitter"];
-      return (
-        <React.Fragment>
-          <div className="logo">
+      return <React.Fragment>
+          <div className="logo-screen">
             <h1>AcmeStack</h1>
             <div className="logo-icon">
-              <img src="/images/svg/logo-acmestack.svg" alt="AcmeStack Logo"/>
+              <img src="/images/svg/logo-acmestack.svg" alt="AcmeStack Logo" />
             </div>
           </div>
-          <div className="login">
+          <div className="login-screen">
             <form className="login-form">
-              <Input type="text" name="username"/>
-              <Input type="password" name="password"/>
-              <Button btnType="Login" click={this.loginUpdate}/>
+              <Input type="text" name="username" />
+              <Input type="password" name="password" ref={this.passwordInput}/>
+              <Button btnType="login" click={this.loginUpdate} login={this.state.loggedIn} />
             </form>
-            <Input type="checkbox" name="remember me"/>
+            <Input type="checkbox" name="remember" text="remember me" />
             <div className="social-media-container">
               <Google />
               <Facebook />
               <Twitter />
             </div>
           </div>
-        </React.Fragment>
-      )
+        </React.Fragment>;
     }
 }
 
